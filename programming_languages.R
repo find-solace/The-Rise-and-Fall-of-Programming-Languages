2f0d1e01ee05571ed7b88638c51a58a6678189a8
find-solace <solaceakello@gmail.com>
2023-12-06 15:58:36 +0300

    popularity of r, dplyr, and ggplot2

--- programming_languages.R
+++ programming_languages.R
@@ -12,3 +12,12 @@ by_tag_year_fraction <- by_tag_year %>% mutate(fraction = number/year_total)
 #Examining trends in popularity of R language
 r_over_time <- by_tag_year_fraction %>% filter(tag == "r")
 ggplot(r_over_time, aes(x = year, y = fraction)) + geom_line()
+
+#Examining trends in r, dplyr, and ggplot2
+selected_packages <- c("r", "dplyr", "ggplot2")
+r_and_packages <- by_tag_year_fraction %>% filter(tag %in% selected_packages)
+
+ggplot(r_and_packages, aes(x = year, y = fraction, color = tag)) + geom_line()
+
+
+
